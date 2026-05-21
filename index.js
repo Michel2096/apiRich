fb60c4a7368075a059b3d0d028518607efda5b06
Michel2096 <al222311739@utvtol.edu.mx>
2026-05-20 20:09:30 -0600

    Nuevo seguimiento

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log("Hola");
-console.log ("Envio este tipo de cambios dentro de mi commit nuevamente");
-console.log ("Ahora");
+const express = require('express')
+const app = express();
+require ('dotenv').config();
+
+app.get ('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () =>{
+    console.log(`Escucha desde el puerto ${PORT}`);
+})
